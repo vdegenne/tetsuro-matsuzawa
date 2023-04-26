e0488614e739a99029476ba8ab031d1ec724a80d
vdegenne <vdegenne@gmail.com>
2023-04-26 15:03:58 +0200

    statics

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,6 @@ import {terser} from 'rollup-plugin-terser';
 
 export default {
   input: 'src/app-container.ts',
-  output: {file: 'public/app-container.js', format: 'iife'},
+  output: {file: 'docs/app-container.js', format: 'iife'},
   plugins: [nodeResolve(), typescript(), terser()]
 }
